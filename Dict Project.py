b7099b41f6e32310ef5dc9737e71559721b3a245
Eric Dixon <eric.dixon@stu.jefferson.kyschools.us>
2018-09-18 10:36:16 -0400

    setting up user interface

--- Dict Project.py
+++ Dict Project.py
@@ -1,5 +1,26 @@
-cowboy_off = {
+#setting up dictionary
+off = {
+   'QB' : '',
+   'RB' : '',
+   'WR_1': '',
+   'WR_2': '',
+   'WR-3': '',
+   'TE_1': '',
+   'LT': '',
+   'LG': '',
+   'C': '',
+   'RG': '',
+   'RT':'',
    
     }
+#making a finction to print the dictionary to look better
+def printdict(dict):
+    for player in off:
+        print(player)
+    return
 
-
+#inforing user how to make and update entries
+name = input("What is your name? ")
+print("Hello %s!" %name)
+print("To make/update a entry in the Offense Dictionary type off.update({('position)' = ('player')})")
+print("Also at anytime type printdict(off) to print out the Offense Dictionary")      
